2e35099e35c004e9539ae5aa8eb3723191c114e4
Ozan ERCAN <ozanercann@gmail.com>
2022-10-24 22:42:52 +0300

    Post  and Home component structure adjusted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import './App.css';
 import {BrowserRouter, Routes , Route,} from "react-router-dom";
-import Post from "./components/Post/Post";
 import Home from "./components/Home/Home";
 import User from "./components/User/User";
 import Navbar from "./components/Navbar/Navbar";
@@ -10,7 +8,6 @@ function App() {
     <div className="App">
       <BrowserRouter>
           <Navbar></Navbar>
-          <Post></Post>
         <Routes >
           <Route exact path="/" element={<Home/>}></Route>
           <Route exact path="/users/:userId" element={<User/>}></Route>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import Post from "../Post/Post";
+import "./Home.css"
 
 export default function Home(){
-    return(
-        <div>Home</div>
-    )
+
+    const [error, setError] = useState(null)
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [postList, setPostList] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:8082/posts")
+            .then(res => res.json())
+            .then((result) => {
+                setIsLoaded(true)
+                setPostList(result)
+            },(error) => {
+                setIsLoaded(true)
+                setError(error)
+            })
+    },[])
+
+    if(error){
+        return <div>Error !!</div>
+    }
+    else if(!isLoaded){
+        return <div>Loading...</div>
+    }
+    else{
+        return(
+            <div className="container">
+                Home
+                {postList.map(post => (
+                    <Post title={post.title} text={post.text}/>
+                ))}
+            </div>
+        )
+    }
 }
